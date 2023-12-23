560dab3936c1dd8bac476896d68bd1223d6b93bc
vth20 <vanthanhhuynhtk@gmail.com>
2023-12-23 17:12:12 +0700

    deploy: fix crash app

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -40,7 +40,7 @@ function Navbar() {
       behavior: "smooth",
     });
   };
- 
+
   return (
     <>
       {/* <div
--- frontend/src/redux/actions/event.js
+++ frontend/src/redux/actions/event.js
@@ -16,7 +16,7 @@ export const createevent = (data) => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: "eventCreateFail",
-      payload: error.response.data.message,
+      payload: error?.response?.data?.message,
     });
   }
 };
@@ -36,7 +36,7 @@ export const getAllEventsShop = (id) => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: "getAlleventsShopFailed",
-      payload: error.response.data.message,
+      payload: error?.response?.data?.message,
     });
   }
 };
@@ -62,7 +62,7 @@ export const deleteEvent = (id) => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: "deleteeventFailed",
-      payload: error.response.data.message,
+      payload: error?.response?.data?.message,
     });
   }
 };
@@ -82,7 +82,7 @@ export const getAllEvents = () => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: "getAlleventsFailed",
-      payload: error.response.data.message,
+      payload: error?.response?.data?.message,
     });
   }
 };
