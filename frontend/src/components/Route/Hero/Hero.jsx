ee3c6cdad53308831dc703340dec43d97da69489
Le Van Tien <lvt030501@gmail.com>
2023-12-26 00:58:03 +0700

    fixbug:  direct link from home page to categories

--- frontend/src/components/Route/Hero/Hero.jsx
+++ frontend/src/components/Route/Hero/Hero.jsx
@@ -15,28 +15,28 @@ const Hero = () => {
         <div className="container">
           <div className="grid-container">
             <div className="featured grid-one">
-              <Link to="categories/furnitures">
+              <Link to="categories">
                 <div id="img1" className="lil-overlay"></div>
                 <img src={Main1} alt="img1" />
                 <p className="main-description">Live Comfortably</p>
               </Link>
             </div>
             <div className="featured grid-two">
-              <Link to="categories/skin-care">
+              <Link to="categories">
                 <div id="img2" className="lil-overlay"></div>
                 <img src={Main2} alt="img2" />
                 <p className="main-description">Skincare</p>
               </Link>
             </div>
             <div className="featured grid-four">
-              <Link to="categories/kitchen">
+              <Link to="categories">
                 <div id="img3" className="lil-overlay"></div>
                 <img src={Main3} alt="img3" />
                 <p className="main-description">Kitchen</p>
               </Link>
             </div>
             <div className="featured grid-four-low">
-              <Link to="categories/electronics">
+              <Link to="categories">
                 <div id="img4" className="lil-overlay"></div>
                 <img src={Main4} alt="img4" />
                 <p className="main-description">Electronics</p>
